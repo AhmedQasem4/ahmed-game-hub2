3ebfafc827a9c96fc057449247af547f648521a7
ahmed Qasem <abok2ssem14@gmail.com>
2023-09-21 16:17:08 +0300

    Refactoring - fetching data using react query.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
-
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "c302c1502fa14e229892f54cfcef41ea",
   },
 });
